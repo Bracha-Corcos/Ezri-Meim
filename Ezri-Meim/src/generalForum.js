80a74120f96e607764a664dc6c0e7e23560b5de7
Ortal Nisim <o.n0533064917@gmail.com>
2024-07-17 19:44:21 +0300

    update 17.7 19:44

---
+++ Ezri-Meim/src/generalForum.js
@@ -0,0 +1,12 @@
+// GeneralForum.js
+import React from 'react';
+
+const GeneralForum = () => {
+  return (
+    <div>
+      <h1>Welcome to the General Forum</h1>
+    </div>
+  );
+};
+
+export default GeneralForum;
